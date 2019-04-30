210141bf95f2497f15553130f1818ac0b5bc5fdb
Zeru Tadesse <36458627+Zeru-Tadesse@users.noreply.github.com>
2019-04-30 19:49:39 -0400

    Update Course.java

--- Course.java
+++ Course.java
@@ -1,4 +1,23 @@
+import java.util.HashSet;
+import java.util.Set;
 
 public class Course {
 
+	private HashSet<Course> prereqs;
+	private HashSet<Course> postreqs;
+	private HashSet<Course> coreqs;
+	private final Subject sub;
+	private final int ID;
+
+	public Course(Subject sub, int id) {
+		this.sub = sub;
+		this.ID = id;
+	}
+	
+	public Course(Subject sub, int id, HashSet<Course> prereqs, HashSet<Course> postreqs, HashSet<Course> coreqs) {
+		this(Subject.CS,101);
+		this.prereqs = prereqs;
+		this.postreqs = postreqs;
+		this.coreqs = coreqs;
+	}
 }
