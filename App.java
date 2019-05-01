57f89c2dddcbc9e4df4b375837117674cc1bde71
castroaj <castroaj@dukes.jmu.edu>
2019-04-30 20:47:09 -0400

    Expanded Course

--- App.java
+++ App.java
@@ -2,6 +2,10 @@
 public class App {
 	public static void main(String[] args)
 	{
+		CourseGraph graph = new CourseGraph();
+		Course cs159 = new Course(Subject.CS, 159);
+		graph.addNode(cs159);
+		System.out.println(graph.numNodes());
 		
 	}
 }
--- Course.java
+++ Course.java
@@ -3,21 +3,67 @@ import java.util.Set;
 
 public class Course {
 
-	private HashSet<Course> prereqs;
-	private HashSet<Course> postreqs;
-	private HashSet<Course> coreqs;
-	private final Subject sub;
-	private final int ID;
+	private HashSet<Course> myPrereqs;
+	private HashSet<Course> myPostreqs;
+	private HashSet<Course> myCoreqs;
+	private final Subject subject;
+	private final int classID;
 
-	public Course(Subject sub, int id) {
-		this.sub = sub;
-		this.ID = id;
+	public Course(Subject subject, int classID) {
+		this.subject = subject;
+		this.classID = classID;
 	}
 	
-	public Course(Subject sub, int id, HashSet<Course> prereqs, HashSet<Course> postreqs, HashSet<Course> coreqs) {
-		this(Subject.CS,101);
-		this.prereqs = prereqs;
-		this.postreqs = postreqs;
-		this.coreqs = coreqs;
+	public Course(Subject sub, int classID, HashSet<Course> prereqs, HashSet<Course> postreqs, HashSet<Course> coreqs) {
+		this.subject = sub;
+		this.classID = classID;
+		this.myPrereqs = prereqs;
+		this.myPostreqs = postreqs;
+		this.myCoreqs = coreqs;
 	}
+	
+	public void addPrereq(Course newPrereq)
+	{
+		if (newPrereq != null)
+		myPrereqs.add(newPrereq);
+	}
+	
+	public void addPostreq(Course newPostreq)
+	{
+		if (newPostreq != null)
+		myPostreqs.add(newPostreq);
+	}
+	
+	public void addCoreq(Course newCoreq)
+	{
+		if (newCoreq != null)
+		myCoreqs.add(newCoreq);
+	}
+	
+	public HashSet<Course> getPrereqs()
+	{
+		return myPrereqs;
+	}
+	
+	public HashSet<Course> getPostreqs()
+	{
+		return myPostreqs;
+	}
+	
+	public HashSet<Course> getCoreqs()
+	{
+		return myCoreqs;
+	}
+	
+	public Subject getSubject()
+	{
+		return subject;
+	}
+	
+	public int getClassID()
+	{
+		return classID;
+	}
+	
+	
 }
