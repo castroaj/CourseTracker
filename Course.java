47e3acda662aa7c453096d23fd0e15eb79943e5e
Zeru Tadesse <36458627+Zeru-Tadesse@users.noreply.github.com>
2019-05-02 12:46:38 -0400

    Adjusted to string for better formatting Created 'priority': equal to the number of required-for classes. The more classes that have it as a prereq the higher prioirty it will get when deciding a schedule

--- Course.java
+++ Course.java
@@ -1,6 +1,11 @@
 import java.util.HashSet;
 import java.util.Set;
 
+/**
+ * 
+ * @author Alex Castro, Zeru Tadesse
+ *
+ */
 public class Course {
 
 	private HashSet<Course> requiredFor;
@@ -12,6 +17,7 @@ public class Course {
 	private boolean offeredFall;
 	private boolean offeredSpring;
 	private boolean isElective;
+	private int priority;
 
 //	public Course(Subject subject, String classID, boolean fall, boolean spring, boolean isElective) {
 //		this.subject = subject;
@@ -23,44 +29,28 @@ public class Course {
 //		this.myCoreqs = new HashSet<Course>();
 //		this.credits = 3;
 //	}
-	
+
 	public Course(Subject subject, String classID, boolean fall, boolean spring) {
-		this.subject = subject;
-		this.classID = classID;
-		this.offeredFall = fall;
-		this.offeredSpring = spring;
-		this.isElective = isElective;
-		this.requiredFor = new HashSet<Course>();
-		this.myCoreqs = new HashSet<Course>();
-		this.credits = 3;
+
+		this(subject, classID, fall, spring, 3);
 	}
+
 	public Course(Subject subject, String classID, boolean fall, boolean spring, int credits) {
 		this.subject = subject;
 		this.classID = classID;
 		this.offeredFall = fall;
 		this.offeredSpring = spring;
-		this.isElective = isElective;
 		this.requiredFor = new HashSet<Course>();
 		this.myCoreqs = new HashSet<Course>();
 		this.credits = credits;
+		this.priority = 0;
 	}
-	
-	public Course(Subject sub, String classID, HashSet<Course> prereqs, HashSet<Course> coreqs, boolean fall,
-			boolean spring, boolean isElective) {
-		this.subject = sub;
-		this.classID = classID;
-		this.requiredFor = prereqs;
-		this.myCoreqs = coreqs;
-		this.offeredFall = fall;
-		this.offeredSpring = spring;
-		this.isElective = isElective;
-		this.credits = 3;
 
-	}
-	
 	public void addPostreq(Course newPostreq) {
-		if (newPostreq != null)
+		if (newPostreq != null) {
 			requiredFor.add(newPostreq);
+			this.priority++;
+		}
 	}
 
 	public void addCoreq(Course newCoreq) {
@@ -89,34 +79,48 @@ public class Course {
 	}
 
 	public String toString() {
+		return toString(true);
+	}
 
-		String s = "\t" + this.subject + this.classID + " (" + this.credits + ") Offered in ";
-		if (offeredFall) {
-			s += "FALL";
-		}
-		if (offeredFall && offeredSpring) {
-			s += " and ";
-		}
-		if (offeredSpring) {
-			s += "SPRING";
-		}
-		if (!offeredFall || !offeredSpring) {
-			s += "\t";
-		}
-		if (requiredFor.size() > 0) {
-			s += " \tRequired for: ";
-			for (Course c : requiredFor) {
-				s += c.toStringSimple();
-				if (requiredFor.size() > 1)
-					s += ", ";
+	public String toString(boolean verbose) {
+		if (verbose) {
+			String s = "\t[" + String.format("%02d", this.priority) + "]" + this.subject + this.classID + " ("
+					+ this.credits + ") Offered in ";
+			if (offeredFall) {
+				s += "Fa";
+			}
+			if (offeredFall && offeredSpring) {
+				s += "-";
+			}
+			if (offeredSpring) {
+				s += "Sp";
+			}
+			if (!offeredFall || !offeredSpring) {
+				s += "\t";
+			}
+			if (requiredFor.size() > 0) {
+				s += " \tRequired for: ";
+				for (Course c : requiredFor) {
+					s += c.toStringSimple();
+					if (requiredFor.size() > 1)
+						s += ", ";
+
+				}
+				if (requiredFor.size() > 1) {
+					s = s.substring(0, s.length() - 2);
+
+				}
 
 			}
+			return s + "\n";
+		} else {
+			return "\t" + toStringSimple() + "\n";
 		}
-		return s + "\n";
+
 	}
 
 	public String toStringSimple() {
-		String s =  this.subject + this.classID + "(" + this.credits +")";
+		String s = this.subject + this.classID;
 		return s;
 	}
 }
