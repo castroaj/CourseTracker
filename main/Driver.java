a8eff4031c1fd6059ddf64d3a7c3cc2efdf40970
Zeru Tadesse <36458627+Zeru-Tadesse@users.noreply.github.com>
2019-05-06 14:07:55 -0400

    Changed main for easy testing

--- main/Driver.java
+++ main/Driver.java
@@ -5,6 +5,7 @@ import java.util.HashSet;
 import java.util.Scanner;
 
 import course_map.*;
+import gui.PreferencesSetupScreen;
 import gui.StartScreen;
 
 public class Driver {
@@ -17,14 +18,22 @@ public class Driver {
 	 * @param args
 	 * @throws IOException
 	 */
-	
-	
+
 	public static void main(String[] args) throws IOException {
-		runGui();
-		//runPlanner();
-		//parseProgram("gen_ed");
-		//parseProgram("cs_major");
-		//parseProgram("cis_minor");
+		// runGui();
+		Generator.loadCourseDatabase("resources/courses.zagbase");
+
+		Planner test = new Planner();
+		test.setName("test");
+		test.setSemester(Semester.FR_FA);
+		test.addProgram(parseProgram("gen_ed"));
+		test.addProgram(parseProgram("cs_major"));
+		test.addProgram(parseProgram("cis_minor"));
+		PreferencesSetupScreen prefScreen = new PreferencesSetupScreen("Test", test);
+		// runPlanner();
+		// parseProgram("gen_ed");
+		// parseProgram("cs_major");
+		// parseProgram("cis_minor");
 	}
 
 	public static void runGui() {
@@ -46,24 +55,21 @@ public class Driver {
 	public static Program parseProgram(String file) {
 		Generator.loadCourseDatabase("resources/courses.zagbase");
 		Program p = Generator.loadProgram("resources/" + file + ".zagp");
-		//System.out.println(p.toString(true));
+		// System.out.println(p.toString(true));
 		return p;
 	}
-	
+
 	public static void runPlanner() {
 		HashSet<Program> programs = new HashSet<Program>();
-		
+
 		programs.add(parseProgram("gen_ed"));
 		programs.add(parseProgram("cs_major"));
 		programs.add(parseProgram("cis_minor"));
 
-		Planner plan = new Planner("Zeru",Semester.FR_FA, programs, 120);
-		
+		Planner plan = new Planner("Zeru", Semester.FR_FA, programs, 120);
+
 		System.out.println(plan);
 
-		
 	}
-	
-	
 
 }
